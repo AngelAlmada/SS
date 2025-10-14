954c46b4e1e73503fed20860bff9e4e7364145bf
AngelAlmada <angelvirgo2000@gmail.com>
2025-10-14 11:23:22 -0700

    servcio de firebase con funciones para login y firestore

---
+++ environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAw5CoTChdM2R7gXk1Rdn_Pn572Fj30BCY", // clave API
+    authDomain: "serviciosocialferrusquilla.firebaseapp.com",
+    projectId: "serviciosocialferrusquilla",
+    storageBucket: "serviciosocialferrusquilla.firebasestorage.app",
+    messagingSenderId: "1017199020011",
+    appId: "1:1017199020011:web:36aeb8a8399a214bbc3bae"
+  }
+};
--- src/app/service/firebase/firebase.ts
+++ src/app/service/firebase/firebase.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { addDoc, collection, Firestore, getDocs } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-firebase',
@@ -7,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './firebase.css'
 })
 export class Firebase {
+  constructor(private auth: Auth, private firestore: Firestore) {}
 
+  async login (email: string, password: string) {
+    return await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async register (email: string, password: string) {
+    return await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async logout () {
+    return await signOut(this.auth);
+  }
+
+  async addDocument(collectionName: string, data: any) {
+    const colRef = collection(this.firestore, collectionName);
+    return await addDoc(colRef, data);
+  }
+
+  async getDocuments(collectionName: string) {
+    const colRef = collection(this.firestore, collectionName);
+    const snapshot = await getDocs(colRef);
+    return snapshot.docs.map(doc => doc.data);
+  }
 }
