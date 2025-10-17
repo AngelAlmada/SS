f95dac67632985f45d9befac99d91a8ce8bb2612
Jona_Balles <JonatanBenjaminx7@gmail.com>
2025-10-16 17:27:25 -0700

    Actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { TestGenerateRegisterComponent } from './components/test-generate-register.component/test-generate-register.component';
-
+import { CuadriculaCursos } from './components/cuadricula-cursos/cuadricula-cursos'
 export const routes: Routes = [
 
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'cursos',
+    component: CuadriculaCursos 
   },
   {
     path: 'testfirebase',
---
+++ src/app/components/cuadricula-cursos/cuadricula-cursos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuadricula-cursos',
+  imports: [],
+  templateUrl: './cuadricula-cursos.html',
+  styleUrl: './cuadricula-cursos.css'
+})
+export class CuadriculaCursos {
+
+}
