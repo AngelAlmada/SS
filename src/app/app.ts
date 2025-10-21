d603d5f3da3dbae1a4efb440a6c5b3b2f7270624
JulioCfy5 <juliocesarfy5@gmail.com>
2025-10-20 22:54:49 -0700

    componente de curso

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componenteprincipal } from './components/componenteprincipal/componenteprincipal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componenteprincipal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
